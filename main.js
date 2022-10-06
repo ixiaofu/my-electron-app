fe9a44224ea845689d53367a7e857d4ca40c475f
Frank <289424358@qq.com>
2022-10-06 21:06:58 +0800

    add build tools

--- main.js
+++ main.js
@@ -1,8 +1,14 @@
-const { app, BrowserWindow } = require('electron');
+const { app, dialog, Tray, BrowserWindow } = require('electron');
 const path = require('path');
+// auto update
+// note:
+// code must be signed
+require('update-electron-app')();
 
+// create window
 function createWindow () {
   const win = new BrowserWindow({
+    icon: path.join(__dirname, 'build/icons/icon.icns'),
     width: 800,
     height: 600,
     webPreferences: {
@@ -12,6 +18,16 @@ function createWindow () {
   win.loadFile('index.html');
 }
 
+// register url
+if (process.defaultApp) {
+  if (process.argv.length >= 2) {
+    app.setAsDefaultProtocolClient('my-electron-app', process.execPath, [path.resolve(process.argv[1])]);
+  }
+} else {
+  app.setAsDefaultProtocolClient('my-electron-app');
+}
+
+// ready
 app.whenReady().then(() => {
   createWindow();
 
@@ -22,6 +38,12 @@ app.whenReady().then(() => {
   });
 });
 
+// on window all closed
 app.on('window-all-closed', function () {
   if (process.platform !== 'darwin') app.quit()
+})
+
+// on open url
+app.on('open-url', (event, url) => {
+   dialog.showErrorBox('Welcome to back', `from ${url}`);
 })
